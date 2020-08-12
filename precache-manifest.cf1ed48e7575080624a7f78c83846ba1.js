852aa486fdf70dd48a16baeb256ad98b083d3e28
Vovasef <vovasef@gmail.com>
2020-08-12 21:04:11 +0300

    Updates

---
+++ precache-manifest.cf1ed48e7575080624a7f78c83846ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0712edfe1f8ad8a4b3d2235a7c455cfe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7dde30d69ece2f59d30",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "259efcf119c44bb14090",
+    "url": "./static/js/2.2ded484a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2ded484a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7dde30d69ece2f59d30",
+    "url": "./static/js/main.1ccbf74a.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
